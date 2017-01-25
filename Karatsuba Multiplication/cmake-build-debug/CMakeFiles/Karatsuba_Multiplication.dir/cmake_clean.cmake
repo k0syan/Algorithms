1638853644de0d4d63e1fb097c8e0b4ae81967fe
shahenkosyan <shahen.qosyan@gmail.com>
2017-01-25 14:37:32 -0500

    Karatsuba Multiplication

---
+++ Karatsuba Multiplication/cmake-build-debug/CMakeFiles/Karatsuba_Multiplication.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Karatsuba_Multiplication.dir/main.cpp.o"
+  "Karatsuba_Multiplication.pdb"
+  "Karatsuba_Multiplication"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Karatsuba_Multiplication.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
