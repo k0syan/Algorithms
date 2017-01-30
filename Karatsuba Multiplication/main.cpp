fd3d97bfec289da4834bb4dd355cb19b6bb4502a
Shahen Kosyan <k0syan@users.noreply.github.com>
2017-01-30 15:15:22 -0500

    Update main.cpp

--- Karatsuba Multiplication/main.cpp
+++ Karatsuba Multiplication/main.cpp
@@ -19,6 +19,7 @@ unsigned long long karatsuba_mul(unsigned long long x, unsigned long long y) {
 }
 
 int main() {
+  //TODO: Find out a type for 64 digit integers
   //unsigned long long x = 3141592653589793238462643383279502884197169399375105820974944592;
   //unsigned long long y = 2718281828459045235360287471352662497757247093699959574966967627;
 
