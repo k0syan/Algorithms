24fb38c45949ec707b8e6047696ba37a7b0cff62
shahenkosyan <shahen.qosyan@gmail.com>
2017-02-05 10:32:24 -0500

    Number of Inversions - Done

--- Number of Inversions/main.cpp
+++ Number of Inversions/main.cpp
@@ -4,72 +4,69 @@
 
 using namespace std;
 
-int inversions_count = 0;
+unsigned long long inversions_count = 0;
 
 vector<int> merge(vector<int> first_array, vector<int> second_array) {
-    vector<int> merged;
+  vector<int> merged;
 
-//    1, 3, 5
-//    2, 4, 6
-
-    int j = 0;
-    int i = 0;
-    for (int k = 0; k < (int)first_array.size() * 2; k++) {
-        if (i < (int)first_array.size()) {
-            if (j < (int)second_array.size()) {
-                if (first_array[i] <= second_array[j]) {
-                    merged.push_back(first_array[i]);
-                    i++;
-                } else {
-                    inversions_count += first_array.size() - i;
-                    merged.push_back(second_array[j]);
-                    j++;
-                }
-            } else {
-                merged.push_back(first_array[i]);
-                i++;
-            }
+  int j = 0;
+  int i = 0;
+  for (int k = 0; k < (int) (first_array.size() + second_array.size()); k++) {
+    if (i < (int) first_array.size()) {
+      if (j < (int) second_array.size()) {
+        if (first_array[i] <= second_array[j]) {
+          merged.push_back(first_array[i]);
+          i++;
         } else {
-            merged.push_back(second_array[j]);
-            j++;
+          inversions_count += first_array.size() - i;
+          merged.push_back(second_array[j]);
+          j++;
         }
+      } else {
+        merged.push_back(first_array[i]);
+        i++;
+      }
+    } else {
+      merged.push_back(second_array[j]);
+      j++;
     }
+  }
 
-    return merged;
+  return merged;
 }
 
-void numberOfInversions(vector<int> numbers, int start, int end) {
+vector<int> merge_sort(vector<int> numbers, int start, int end) {
+  if (end == start) {
+    return vector<int>{numbers[start]};
+  }
 
-    if (numbers.size() == 2) {
-        cout << "yaaay" << endl;
-    }
+  if (end - start < 2) {
+    vector<int> first_array = {numbers[start]};
+    vector<int> second_array = {numbers[end]};
+    return merge(first_array, second_array);
+  }
 
-
+  return merge(merge_sort(numbers, start, (end + start) / 2), merge_sort(numbers, (end + start) / 2 + 1, end));
 }
+
 
 int main() {
-//    int *numbers = new int[100000];
-//    int j = 0;
+  ifstream file("../IntegerArray.txt");
 
-    vector<int> numbers = {1, 3, 5};
-    vector<int> numbers1 = {2, 4, 6};
+  int data;
+  vector<int> numbers;
 
-    vector<int> merged = merge(numbers, numbers1);
+  while (file >> data) {
+    numbers.push_back(data);
+  }
 
-    for (int i = 0; i < merged.size(); i++) {
-        cout << merged[i] << " ";
-    }
-    cout << inversions_count << endl;
+  vector<int> merged = merge_sort(numbers, 0, (int) numbers.size() - 1);
 
-//    ifstream file("../IntegerArray.txt");
-//
-//    int data;
-//
-//    while (file >> data) {
-//        numbers.push_back(data);
-//    }
-//
-//    numberOfInversions(numbers, 0, (int)numbers.size() - 1);
+  for (int i = 0; i < merged.size(); ++i) {
+    cout << merged[i] << endl;
+  }
 
-    return 0;
+  cout << endl << "Inversions count in your array is: " << inversions_count << endl;
+
+  return 0;
 }
