4f65e4f374be58749a2f1e696e76087dce8cdfc9
shahenkosyan <shahen.qosyan@gmail.com>
2017-02-13 19:02:40 +0400

    Implemented read from file functionality

--- Quick Sort/main.cpp
+++ Quick Sort/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  ifstream file("../QuickSort.txt");
+
+  int data;
+  vector<int> numbers;
+
+  while (file >> data) {
+    numbers.push_back(data);
+  }
+
+  for (int i = 0; i < numbers.size(); ++i) {
+    cout << numbers[i] << endl;
+  }
+
   return 0;
 }
