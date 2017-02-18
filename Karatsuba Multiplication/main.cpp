25c8a75d5cd409cadbaa3929b3bc5aff9b9bd83f
Shahen Kosyan <k0syan@users.noreply.github.com>
2017-02-18 16:50:06 -0500

    Update main.cpp

--- Karatsuba Multiplication/main.cpp
+++ Karatsuba Multiplication/main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <cmath>
 
+typedef unsigned long long huge;
 
-unsigned long long karatsuba(unsigned long long x, unsigned long long y) {
+huge karatsuba(huge x, huge y) {
   int len_x = (int) std::to_string(x).length();
 
   if (len_x == 2) {
     return x * y;
   } else {
-    unsigned long long a = (unsigned long long) (x / pow(10, len_x / 2));
-    unsigned long long b = x % (unsigned long long) (pow(10, len_x / 2));
-    unsigned long long c = (unsigned long long) (y / pow(10, len_x / 2));
-    unsigned long long d = y % (unsigned long long) (pow(10, len_x / 2));
+    huge a = (huge) (x / pow(10, len_x / 2));
+    huge b = x % (huge) (pow(10, len_x / 2));
+    huge c = (huge) (y / pow(10, len_x / 2));
+    huge d = y % (huge) (pow(10, len_x / 2));
 
-    return (unsigned long long) (pow(10, len_x) * karatsuba(a, c) +
-                                 pow(10, len_x / 2) * (karatsuba(a, d) + karatsuba(b, c)) +
-                                 karatsuba(b, d));
+    return (huge) (pow(10, len_x) * karatsuba(a, c) +
+                   pow(10, len_x / 2) * (karatsuba(a, d) + karatsuba(b, c)) +
+                   karatsuba(b, d));
   }
 }
 
 int main() {
-  unsigned long long x = 1234;
-  unsigned long long y = 5678;
-  unsigned long long answer = karatsuba(x, y);
+  huge x = 1234;
+  huge y = 5678;
+  huge answer = karatsuba(x, y);
 
   std::cout << answer << std::endl;
 
